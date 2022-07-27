f724b52dcedba51d8e991c3765f38a96589fd46b
Deepa <deepa.x.r@gmail.com>
2022-07-27 17:58:30 -0400

    bug fixes

--- synmax/common/api_client.py
+++ synmax/common/api_client.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 
 from synmax.common.model import PayloadModelBase
 
+logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 _api_timeout = 600
@@ -23,8 +24,8 @@ class ApiClient:
 
         # HTTPAdapter
         retry_strategy = Retry(
-            total=5,
-            backoff_factor=1,
+            total=8,
+            backoff_factor=2,
             status_forcelist=[408, 429, 500, 502, 503, 504, 505],
             method_whitelist=["HEAD", "GET", "PUT", "DELETE", "OPTIONS", "TRACE"],
 
@@ -49,11 +50,17 @@ class ApiClient:
         :param return_json:
         :return:
         """
-        response.raise_for_status()
+        # response.raise_for_status()
+        if not response.ok:
+            # logging.error('Error in response. %s')
+            return None
+
         if return_json:
             json_data = response.json()
             if 'error' in json_data:
-                raise Exception(json_data['error'])
+                # raise Exception(json_data['error'])
+                logging.error(json_data['error'])
+                return None
             return json_data
 
         return response
@@ -73,8 +80,12 @@ class ApiClient:
         LOGGER.info(url)
         response = self.session.get(url, params=params, timeout=_api_timeout, **kwargs)
         json_result = self._return_response(response, return_json)
-        df = pandas.DataFrame(json_result['data'])
-        return df
+
+        if json_result:
+            df = pandas.DataFrame(json_result['data'])
+            return df
+
+        return None
 
     def post(self, url, payload: PayloadModelBase = None, return_json=False, **kwargs) -> pandas.DataFrame:
         r"""Sends a POST request.
@@ -108,15 +119,17 @@ class ApiClient:
 
         with tqdm(desc=F"Querying API {url} pages", total=total_pages, dynamic_ncols=True, miniters=0) as progress_bar:
             while total_count >= pagination['start'] + pagination['page_size']:
-                progress_bar.refresh()
-                payload.pagination_start = pagination['start'] + pagination['page_size']
-                response = self.session.post(url, data=payload.payload(), timeout=_api_timeout, **kwargs)
-                json_result = self._return_response(response, return_json)
+                try:
+                    progress_bar.refresh()
+                    payload.pagination_start = pagination['start'] + pagination['page_size']
+                    response = self.session.post(url, data=payload.payload(), timeout=_api_timeout, **kwargs)
+                    json_result = self._return_response(response, return_json)
 
-                pagination = json_result['pagination']
-                data_list.extend(json_result['data'])
-                progress_bar.update()
-
+                    pagination = json_result['pagination']
+                    data_list.extend(json_result['data'])
+                    progress_bar.update()
+                except:
+                    pass
         payload.pagination_start = 0
         LOGGER.info('Total response data: %s', len(data_list))
         df = pandas.DataFrame(data_list)
