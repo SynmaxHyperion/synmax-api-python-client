8c50e6c582dd43ce24a398a4e1e5350a94792921
flxshk <92094562+flxshk@users.noreply.github.com>
2023-04-23 23:09:49 +0200

    saving changes for next version

--- synmax/common/model.py
+++ synmax/common/model.py
@@ -12,8 +12,8 @@ class PayloadModelBase:
     region: str = None
     sub_region: str = None
     operator: str = None
-
-    # fetch_all = True
+    api: list = None
+    aggregate_by: str = None
 
     def payload(self, pagination_start=None):
         # just intercept the payload calls so they aren't relayed to `object`
