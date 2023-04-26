4831697c9183cbcabf236cf688245d88c2a74f33
felix <felix.schrock3@gmail.com>
2023-04-26 16:15:11 +0000

    revert changes temporarily

--- synmax/hyperion/hyperion_client.py
+++ synmax/hyperion/hyperion_client.py
@@ -46,13 +46,12 @@ class HyperionApiClient(object):
 
         if access_token is None:
             access_token = os.getenv('access_token')
-        self.access_key = access_token
 
-        # for each previous client version add new elif statement with respective cloud run address for backward compatibility
+        self.access_key = access_token
         if local_server:
             self._base_uri = 'http://127.0.0.1:8080/'
         else:
-            self._base_uri = 'https://hyperion.api.synmax.com/' # v2 temp location is at: 'https://hyperion-api-v2-xzzxclvs3q-uc.a.run.app'
+            self._base_uri = 'https://hyperion.api.synmax.com/'
 
         if async_client:
             LOGGER.info('Initializing async client')
@@ -65,53 +64,46 @@ class HyperionApiClient(object):
     # GET
 
     def fetch_regions(self) -> pandas.DataFrame:
-        return self.api_client_sync.get(f"{self._base_uri}/v2/regions", return_json=True)
+        return self.api_client_sync.get(f"{self._base_uri}/regions", return_json=True)
 
     def fetch_operator_classification(self) -> pandas.DataFrame:
-        return self.api_client_sync.get(f"{self._base_uri}/v2/operatorclassification", return_json=True)
-
-    # Legacy endpoints of v1 API (after v1, daily_fracked_feet and long_term_forecast are POST endpoints)
-    def fetch_daily_fracked_feet(self) -> pandas.DataFrame:
-        return self.api_client_sync.get(f"{self._base_uri}/dailyfrackedfeet", return_json=True)
+        return self.api_client_sync.get(f"{self._base_uri}/operatorclassification", return_json=True)
 
     def fetch_long_term_forecast(self) -> pandas.DataFrame:
         return self.api_client_sync.get(f"{self._base_uri}/longtermforecast", return_json=True)
-    
+
+    def fetch_daily_fracked_feet(self) -> pandas.DataFrame: 
+        return self.api_client_sync.get(f"{self._base_uri}/dailyfrackedfeet", return_json=True)
 
     # POST
-    def daily_fracked_feet(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/dailyfrackedfeet", payload=payload, return_json=True)
-
-    def long_term_forecast(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/longtermforecast", payload=payload, return_json=True)
 
     def well_completion(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/completions", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/completions", payload=payload, return_json=True)
 
     def ducs_by_operator(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/ducsbyoperator", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/ducsbyoperator", payload=payload, return_json=True)
 
     def frac_crews(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/fraccrews", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/fraccrews", payload=payload, return_json=True)
 
     def production_by_county_and_operator(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/productionbycountyandoperator", payload=payload,
+        return self.api_client.post(f"{self._base_uri}/productionbycountyandoperator", payload=payload,
                                     return_json=True)
 
     def production_by_well(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/productionbywell", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/productionbywell", payload=payload, return_json=True)
 
     def rigs(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/rigs", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/rigs", payload=payload, return_json=True)
 
     def wells(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/wells", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/wells", payload=payload, return_json=True)
 
     def short_term_forecast(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/shorttermforecast", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/shorttermforecast", payload=payload, return_json=True)
 
     def short_term_forecast_history(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/shorttermforecasthistory", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/shorttermforecasthistory", payload=payload, return_json=True)
 
     def daily_production(self, payload: ApiPayload = ApiPayload()) -> pandas.DataFrame:
-        return self.api_client.post(f"{self._base_uri}/v2/dailyproduction", payload=payload, return_json=True)
+        return self.api_client.post(f"{self._base_uri}/dailyproduction", payload=payload, return_json=True)
